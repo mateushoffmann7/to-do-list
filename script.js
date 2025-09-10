fe19606de407361e9b5762507d8ddfa617db9385
Mettel07 <mateushoffmann0707@gmail.com>
2025-09-10 04:32:57 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const button = document.querySelector(".button-add");
+const input = document.querySelector(".input-box input");
+const list = document.querySelector(".list ul");
+
+const newLi = "";
+
+button.addEventListener("click", () => {
+  if (input.value !== "") {
+    const newLi = document.createElement("li");
+    newLi.classList.add("newLi");
+    newLi.innerHTML = input.value;
+    list.append(newLi);
+    input.value = "";
+  }
+});
